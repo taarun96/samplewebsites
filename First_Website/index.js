d4ad1467ece3957f078cd2d76209346defd2e227
taarun96 <taarun96@gmail.com>
2020-11-20 21:29:52 +0530

    Initial Commit

---
+++ First_Website/index.js
@@ -0,0 +1,26 @@
+https://startbootstrap.github.io/startbootstrap-new-age/img/demo-screen-1.jpg
+
+https://startbootstrap.github.io/startbootstrap-new-age/img/google-play-badge.svg
+https://startbootstrap.github.io/startbootstrap-new-age/img/app-store-badge.svg
+
+https://startbootstrap.github.io/startbootstrap-new-age/img/demo-screen-1.jpg
+
+
+
+mobile
+<i class="fa fa-mobile-alt"></i>
+
+
+<i class="fas fa-camera"></i>
+
+<i class="fas fa-gift"></i>
+
+<i class="fas fa-lock-open"></i>
+
+<i class="fab fa-twitter"></i>
+
+
+
+<i class="fab fa-facebook"></i>
+
+<i class="fab fa-linkedin"></i>
